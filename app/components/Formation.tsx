7289ecf5e20b85bb89d1a33c5930d2a7380ffe05
sgautier-dev <sgautier.dev@gmail.com>
2023-05-30 11:50:39 +0400

    section formation done

--- app/components/Formation.tsx
+++ app/components/Formation.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import { training } from "@/lib/formation";
+import { training } from "@/lib/formations";
+import { diploma } from "@/lib/diplomes";
 
 export default function Formation() {
 	return (
 		<div
-			className="relative isolate overflow-hidden pb-16 pt-14 sm:pb-20 mt-32 sm:mt-56 xl:mx-auto xl:max-w-7xl xl:px-8 text-white"
+			className="relative isolate overflow-hidden pb-16 pt-14 sm:pb-20 mt-32 sm:mt-56 xl:mx-auto xl:max-w-7xl xl:px-8"
 			id="formation"
 		>
 			<Image
@@ -14,14 +15,14 @@ export default function Formation() {
 				height={1907}
 				className="absolute inset-0 -z-10 h-full w-full object-cover"
 			/>
-			<div className="absolute inset-0 bg-darkGrayCust opacity-50"></div>
+			<div className="absolute inset-0 bg-white opacity-80"></div>
 
 			<div className="isolate mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="py-8 sm:py-10 lg:py-12 grid lg:grid-cols-2 mx-auto max-w-3xl lg:max-w-4xl xl:max-w-5xl gap-x-8">
 					<h2 className="text-4xl font-days tracking-tight text-pinkCust sm:text-5xl lg:text-6xl">
 						Ma Formation
 					</h2>
-					<p className="mt-6 text-lg leading-8">
+					<p className="mt-6 text-lg">
 						Coach sportive depuis 2003, je suis titulaire du Brevet d’Etat
 						d’Educateur Sportif Métiers de la Forme, mais aussi formée Les Mills
 						(Body Pump, Body Balance, Body Combat, Body Attack), FFGYM,
@@ -36,7 +37,7 @@ export default function Formation() {
 					<div className="-mx-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-3">
 						{training.map((item) => (
 							<div
-								className="grid grid-cols-2 items-center bg-white/10 p-6 sm:p-10"
+								className="grid grid-cols-2 items-center bg-grayCust/60 p-6 sm:p-10"
 								key={item.name}
 							>
 								<Image
@@ -50,42 +51,20 @@ export default function Formation() {
 							</div>
 						))}
 					</div>
-					<div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-						<img
-							className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-							src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-							alt="Transistor"
-							width={158}
-							height={48}
-						/>
-						<img
-							className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-							src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-							alt="Reform"
-							width={158}
-							height={48}
-						/>
-						<img
-							className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-							src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-							alt="Tuple"
-							width={158}
-							height={48}
-						/>
-						<img
-							className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-							src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-							alt="SavvyCal"
-							width={158}
-							height={48}
-						/>
-						<img
-							className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-							src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-							alt="Statamic"
-							width={158}
-							height={48}
-						/>
+
+					{/* diploma cloud */}
+					<div className="mx-auto mt-10 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+						{diploma.map((item) => (
+							<div key={item.name}>
+								<Image
+									className="col-span-2 max-h-32 w-full object-contain lg:col-span-1"
+									src={item.image}
+									alt={item.name}
+									width={200}
+									height={100}
+								/>
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
---
+++ lib/diplomes.ts
@@ -0,0 +1,26 @@
+export const diploma = [
+	{
+		name: "Yogalates",
+		image: "/images/Certif_YogaLates_Lynda.jpeg",
+	},
+    {
+		name: "Pilates",
+		image: "/images/CERTIFICATION PILATES MATWORK_Lynda.jpg",
+	},
+    {
+		name: "Trx",
+		image: "/images/Dilpôme TRX_Lynda.jpg",
+	},
+    {
+		name: "Aerobic",
+		image: "/images/Diplôme Aerobic pro instructeur_Lynda.jpg",
+	},
+    {
+		name: "DiplomeEtat",
+		image: "/images/Diplome Bret d'Etat1024_Lynda.jpg",
+	},
+    {
+		name: "Fisaf",
+		image: "/images/Diplôme FISAF Basic training instructeur1024_Lynda.jpg",
+	},
+];
