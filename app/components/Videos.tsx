3a74ad5e8c778b5522727f9d25bb448bdf44b826
sgautier-dev <sgautier.dev@gmail.com>
2023-07-20 12:05:46 +0400

    videos working and background added

--- app/components/Videos.tsx
+++ app/components/Videos.tsx
@@ -1,40 +1,24 @@
-const products = [
-	{
-		id: 1,
-		name: "Fusion",
-		category: "UI Kit",
-		href: "#",
-		price: "$49",
-		imageSrc:
-			"https://tailwindui.com/img/ecommerce-images/product-page-05-related-product-01.jpg",
-		imageAlt:
-			"Payment application dashboard screenshot with transaction table, financial highlights, and main clients on colorful purple background.",
-	},
-	// More products...
-];
-
 interface VideosProps {
 	videos: Video[];
 }
 
 export default function Videos({ videos }: VideosProps) {
 	return (
-		<div className="bg-white">
-			<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-				<div className="flex items-center justify-between space-x-4">
-					<h2 className="text-lg font-medium text-gray-900">
-						Les vidéos disponibles
-					</h2>
-				</div>
-				<div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
-					{videos.map((video) => (
-						<div key={video._id} className="group relative">
-							<div className="aspect-h-3 aspect-w-4 overflow-hidden rounded-lg bg-gray-100">
-								<video controls className="object-cover object-center">
-									<source src={video.videoUrl} />
-									Votre navigateur ne prend pas en charge le lecteur vidéo.
-								</video>
-								{/* <div
+		<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+			<div className="flex items-center justify-between space-x-4">
+				<h2 className="text-lg font-medium text-gray-900">
+					Les vidéos disponibles
+				</h2>
+			</div>
+			<div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+				{videos.map((video) => (
+					<div key={video._id} className="group relative">
+						<div className="aspect-h-3 aspect-w-4 overflow-hidden rounded-lg bg-gray-100">
+							<video controls className="object-cover object-center">
+								<source src={video.videoUrl} type="video/mp4" />
+								Votre navigateur ne prend pas en charge le lecteur vidéo.
+							</video>
+							{/* <div
 									className="flex items-end p-4 opacity-0 group-hover:opacity-100"
 									aria-hidden="true"
 								>
@@ -42,20 +26,14 @@ export default function Videos({ videos }: VideosProps) {
 										Visionner la vidéo
 									</div>
 								</div> */}
-							</div>
-							<div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900">
-								<h3>
-									<a href="#">
-										<span aria-hidden="true" className="absolute inset-0" />
-										{video.title}
-									</a>
-								</h3>
-								<p>{video.description}</p>
-							</div>
-							<p className="mt-1 text-sm text-gray-500">{video.length}</p>
 						</div>
-					))}
-				</div>
+						<div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-pinkCust">
+							<h3>{video.title}</h3>
+							<p>{video.length}</p>
+						</div>
+						<p className="mt-1 text-sm">{video.description}</p>
+					</div>
+				))}
 			</div>
 		</div>
 	);
--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,15 +1,34 @@
 import Videos from "../components/Videos";
 import getVideos from "@/sanity/lib/getVideos";
+import Image from "next/image";
 
 export default async function Members() {
-    const videos = await getVideos();
+	const videos = await getVideos();
 
 	return (
 		<main className="relative mx-auto min-h-screen font-judson">
+			<Image
+				src="/images/felipe-correia-ScQngs6oO1E-unsplash-1920X1280.jpg"
+				alt="Lynda votre coach fitness à la réunion"
+				width={1920}
+				height={1280}
+				sizes="(max-width: 640px) 640px, (max-width: 768px) 768px, (max-width: 1024px) 1024px, 1280px"
+				className="absolute inset-0 -z-10 h-full w-full object-cover"
+			/>
+			<div className="absolute inset-0 bg-white opacity-80"></div>
 			<div className="isolate mx-auto max-w-7xl py-14 px-6 lg:px-8">
-				<h2 className="text-4xl font-days tracking-tight text-pinkCust sm:text-5xl lg:text-6xl capitalize">
-					Espace membres
-				</h2>
+				<div className="py-8 sm:py-10 lg:py-12 grid lg:grid-cols-2 mx-auto max-w-3xl lg:max-w-4xl xl:max-w-5xl gap-x-8">
+					<h2 className="text-4xl font-days tracking-tight text-pinkCust sm:text-5xl lg:text-6xl capitalize">
+						Espace membres
+					</h2>
+					<p className="mt-6 text-lg">
+						Ici, le sport ne s&apos;arrête jamais. Profitez de vos séances
+						d&apos;entraînement préférées - Stretching, Pilates, Yogalates,
+						Cardio et plus encore - à tout moment, n&apos;importe où. Ne laissez
+						jamais vos vacances interrompre votre rythme, restez en forme avec
+						LFIT. Bon entrainement!
+					</p>
+				</div>
 				<Videos videos={videos} />
 			</div>
 		</main>
