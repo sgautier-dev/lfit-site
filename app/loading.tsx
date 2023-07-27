ef29e40d19175b8885402ebbbf20cdc75533c203
sgautier-dev <sgautier.dev@gmail.com>
2023-07-27 11:39:10 +0400

    dependencies reinstalled, loading page added

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+	return (
+		<div className="grid min-h-screen place-items-center px-6 py-24 sm:py-32 lg:px-8">
+			<h1 className="mt-4 text-3xl font-bold tracking-tight text-pinkCust sm:text-5xl animate-bounce">
+				En cours de chargement...
+			</h1>
+		</div>
+	);
+}
