7baaa51c444d9df17e409f9b13ef081b3e590498
sgautier-dev <sgautier.dev@gmail.com>
2023-09-15 10:51:50 +0400

    cookies modal added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState, useEffect } from "react";
 // import "intersection-observer"; //fallback polyfill in case of some older browser versions
 import Hero from "./components/Hero";
 import Formation from "./components/Formation";
@@ -8,10 +10,21 @@ import Toning from "./components/Toning";
 import Coaching from "./components/Coaching";
 import Contact from "./components/Contact";
 import Planning from "./components/Planning";
+import CookiesMod from "./components/cookiesModal";
 
 export default function Home() {
+	const [showModal, setShowModal] = useState(false);
+
+	useEffect(() => {
+		const cookieConsent = localStorage.getItem("cookieConsent");
+
+		if (cookieConsent === null) {
+			setShowModal(true);
+		}
+	}, []);
 	return (
 		<main className="relative mx-auto min-h-screen font-judson">
+			{showModal && <CookiesMod />}
 			<Hero />
 			<Formation />
 			<Cours />
